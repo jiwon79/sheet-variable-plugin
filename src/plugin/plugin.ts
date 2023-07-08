90ea937d124d4954940a08ae9586012efa4d304c
jiwon79 <19-079@ksa.hs.kr>
2023-07-08 12:04:12 +0900

    :sparkles: sheetURl, name 받아서 plugin으로 전송

--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1,1 +1,9 @@
 figma.showUI(__html__, {width: 300, height: 200});
+
+figma.ui.onmessage = msg => {
+  console.log(msg.type)
+  if (msg.type === 'run') {
+    console.log("run")
+    console.log(msg)
+  }
+}
